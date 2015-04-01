3abdb8d7747204be40f1cf5342ca15ff620a2df6
Brandon Berhent <bbedward@gmail.com>
2015-04-01 00:53:18 -0400

    Move CPU_STARTUP notification to migration_call

--- kernel/sched/bfs.c
+++ kernel/sched/bfs.c
@@ -5615,7 +5615,8 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 #endif
 
 	switch (action & ~CPU_TASKS_FROZEN) {
-
+	case CPU_STARTING:
+		return NOTIFY_OK;
 	case CPU_UP_PREPARE:
 		break;
 
@@ -5670,7 +5671,6 @@ static int sched_cpu_active(struct notifier_block *nfb,
 				      unsigned long action, void *hcpu)
 {
 	switch (action & ~CPU_TASKS_FROZEN) {
-	case CPU_STARTING:
 	case CPU_DOWN_FAILED:
 		set_cpu_active((long)hcpu, true);
 		return NOTIFY_OK;
